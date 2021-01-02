fc7a1babb08fd2098dec57aab828c8d2a172ff13
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-01-02 18:10:01 -0300

    Isolando componentes

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -30,55 +30,21 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      padding: 16px;
-      width: 100%;
-      border: 2px solid #232129;
-      border-radius: 10px;
-      color: #F4EDE8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #FF9000;
-      height: 56px;
-      border-radius : 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312E38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#FF9000')}
-      }
-    }
-
     a {
-      color: #F4EDE8;
+      color: #f4ede8;
       display: block;
       margin-top: 24px;
       text-decoration: none;
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, '#F4EDE8')}
+        color: ${shade(0.2, '#F4EDE8')};
       }
     }
   }
 
   > a {
-    color: #FF9000;
+    color: #ff9000;
     line-height: 21px;
     text-align: center;
 
@@ -90,7 +56,7 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#FF9000')}
+      color: ${shade(0.2, '#FF9000')};
     }
 
     svg {
