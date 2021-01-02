4dc0b404ebad0abd18f3a478f6638a5078fe43d8
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-01-02 20:10:46 -0300

    Pagina de Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyles />
   </>
 );
